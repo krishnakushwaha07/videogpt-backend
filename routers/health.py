c237f8cfde2aba99b1b9f48885875829d206b360
krishna kushwaha <krishnakushwaha2248@gmail.com>
2026-08-26 22:41:05 +0530

    added dummy cookie check route

--- routers/health.py
+++ routers/health.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Request
 
 health_router = APIRouter()
 
 @health_router.get("/", status_code=status.HTTP_200_OK)
 async def health_check():
     return "Server is running fine :)"
+
+
+@health_router.get("/debug-cookie")
+async def debug_cookie(request: Request):
+    return {
+        "cookies": request.cookies
+    }
